81cd51133e1738d98bbd617ff2389b9489d78894
tristankruuk <tristanhans.kruuk@techno.ee>
2026-09-07 14:13:04 +0300

    Add array examples

--- index.php
+++ index.php
@@ -1,25 +1,29 @@
 <?php
-$test = true;
-$test = false;
-$test = true && true; // Vähemalt 1 false = false
-$test = true || false; // Vähemalt 1 true = true
-$test = !false; // Pöörab väärtuse vastupidi
-
-// Kas vastab tõele?
-$test = 10 > 5;
-$test = 10 < 5;
-$test = 10 < 10;
-
-$test = 10 == 10; // Võrdne väärtus
-$test = 10 === 10; // Võrdne väärtus + tüüp
-$test = 10 == '10';
-$test = 10 === '10';
-$test = 10 != 5; // Ei ole võrdne (nagu ==)
-$test = 10 !== 5; // Ei ole võrdne (nagu ===)
-
-$test = 10 >= 10; // Suurem või võrdne
-$test = 10 <= 10; // Väiksem või võrdne
-
-$test = true && false || (true && !true); // Tehe, kasutatakse sisselogimise kontrollimisel
+$test = [1, 2, 'word', true];
+var_dump($test[2]);
+$test[2] = 'asdasda';
 var_dump($test);
+$test[4] = 'new value';
+var_dump($test);
+$test[] = 'another value';
+var_dump($test); 
+array_push($test, 'pushed value');
+var_dump($test);
+$test = [ 
+            'name' => 'Kaspar',
+            'age' => 33,
+            1,
+            3,
+            'vzcxzzxc',
+            100 => 'new value',
+            5,
+        ];
+var_dump($test['name']);
+var_dump($test);
+$test = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+var_dump($test[1][2]);
 ?>
