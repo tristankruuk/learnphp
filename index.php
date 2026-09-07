b92cc3bdba6acd95ef8cc8eeb9a026b5e238339a
tristankruuk <tristanhans.kruuk@techno.ee>
2026-09-07 13:24:15 +0300

    Add variable example

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-echo "Hello, World!"
-// Lühike kommentaar
-/**
- * 
- * Pikem kommentaar
- * 
- */
+echo "Hello, World!";
+
+$name = "John Doe"; // Dollarimärgiga on muutuja
+$age = 30;
+echo $age; // Ka echo sees dollarmärk
+$age = $age * 2 * 4 * 4 * 5 * 6 * 7 * 8 * 9 * 10;
+echo $age;
+echo 2 + 2;
 ?>
