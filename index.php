7591ab37d1f8ec594d81a6550408fc8cd0cd808b
tristankruuk <tristanhans.kruuk@techno.ee>
2026-09-07 14:04:07 +0300

    Add string examples

--- index.php
+++ index.php
@@ -1,24 +1,13 @@
 <?php
-$test = 10 + 2;
-$test = 10 - 2;
-$test = 10 * 2;
-$test = 10 / 2;
-
-$test = $test - 1;
-
-// Samad, aga ei pea välja kirjutama, et x=x-1, vaid kohe võetakse väärtusest maha
-$test -= 1;
-$test += 1;
-$test *= 2;
-$test /= 2;
-
-$test = 10 % 3; // Jääk
-$test++; // Suurendab ühe võrra
-$test--; // Vähendab ühe võrra
-$test = (10 + (2 - 3) * 4) / 2;
-$test %= 2; // Muutuja jääk
-$test **= 3; // Muutuja astendamine
-$test = 2 ** 4; // Astendamine
-$test = sqrt(16); // Ruutjuur
-var_dump($test);
+$test = 'hello';
+$test = 'hello' . ' world'; // Tekstide ühendamine
+$test .= '!'; // Lõppu lisamine
+$name = 'Kaspar';
+$age = 33;
+$test = $name . ' is ' . $age . ' years old.'; // Muutujad tekstis
+$test = "$name is $age years old."; // Muurujad otse teksti sisse
+$test = "hello\n\nworld"; // Uus rida
+$letter = 'a'; 
+$letter++; // Tähestiku järgi järgmine täht
+var_dump($letter);
 ?>
