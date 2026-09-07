360ccee3499494f21d67be584617daaf882b6a5c
tristankruuk <tristanhans.kruuk@techno.ee>
2026-09-07 13:46:06 +0300

    Add variable types

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <?php
-echo "Hello, World!";
+$test = 10; // Integer
+$test = 3.1; // Float
+$test = "Hello World"; // String
+$test = "õpilane";
+$test = "👨"; // 4
+$test = "👨🏿"; // 8
+$test = true; // Boolean
+$test = null; // Väärtuse puudmine (NULL)
+$lol; // Annab warningu (Undefined)
+var_dump($test);
+var_dump($lol);
+$test = [1, 2, "three", true]; // Array ehk nimekiri
+var_dump($test);
+$test = new stdClass(); // Objektiga saab lisada omadusi
+$test->name = "John";
+var_dump($test);
 
-$name = "John Doe"; // Dollarimärgiga on muutuja
-$age = 30;
-echo $age; // Ka echo sees dollarmärk
-$age = $age * 2 * 4 * 4 * 5 * 6 * 7 * 8 * 9 * 10;
-echo $age;
-echo 2 + 2;
+$num1 = 1000000000;
+$num2 = "1000000000";
+var_dump($num1 == $num2); // Kas väärtused on võrdsed?
+var_dump($num1 === $num2); // Kas väärtused + tüübid on võrdsed?
 ?>
