3e79d2cf0a6bbaaacf7a8929957f494cfa86611b
tristankruuk <tristanhans.kruuk@techno.ee>
2026-09-09 08:38:00 +0300

    Add if and switch case

--- index.php
+++ index.php
@@ -1,29 +1,46 @@
 <?php
-$test = [1, 2, 'word', true];
-var_dump($test[2]);
-$test[2] = 'asdasda';
-var_dump($test);
-$test[4] = 'new value';
-var_dump($test);
-$test[] = 'another value';
-var_dump($test); 
-array_push($test, 'pushed value');
-var_dump($test);
-$test = [ 
-            'name' => 'Kaspar',
-            'age' => 33,
-            1,
-            3,
-            'vzcxzzxc',
-            100 => 'new value',
-            5,
-        ];
-var_dump($test['name']);
-var_dump($test);
-$test = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-];
-var_dump($test[1][2]);
+$num = 10;
+
+// Lihtne if
+if($num > 5) {
+    var_dump('Bigger');
+}
+
+if($num > 5) {
+    var_dump('Bigger');
+} else {
+    var_dump('Smaller');
+}
+
+// If koos else if-iga, kui on rohkem tingimusi, siis esimene on if, viimane else ja kõik ülejäänud else if
+if($num > 5) {
+    var_dump('Bigger');
+} else if($num < 5) {
+    var_dump('Smaller');
+} else {
+    var_dump('Equal');
+}
+
+$i = 1;
+
+// Switch case
+switch($i) {
+    case 1:
+        var_dump('yx');
+        break;
+    case 2:
+        var_dump('kaks');
+        break;
+    case 3:
+    case 4:
+        var_dump('kolm neli');
+        break;
+    case 5:
+        var_dump('viis');
+    case 6: 
+        var_dump('kuus');
+        break;
+    default:
+        var_dump('dunno');
+}
 ?>
