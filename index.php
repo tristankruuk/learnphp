c440dffa5f69b8318b2b8395f77a59e1d0755fa8
tristankruuk <tristanhans.kruuk@techno.ee>
2026-09-09 11:57:26 +0300

    Add class, object example (Mature commit)

--- index.php
+++ index.php
@@ -15,5 +15,25 @@ class Box {
     public function close() {
         $this->isOpen = false;
     }
+     public function volume() {
+        return $this->height * $this->length * $this->width;
+    }
 }
+
+
+$box1 = new Box();
+$box1->width = 10;
+$box1->height = 20;
+$box1->length = 30;
+$box1->open();
+var_dump($box1);
+var_dump($box1->volume());
+
+$box2 = new Box();
+$box2->width = 30;
+$box2->height = 40;
+$box2->length = 50;
+var_dump($box2);
+var_dump($box2->volume());
+var_dump($box1);
 ?>
