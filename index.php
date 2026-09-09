84f69a7a180257bc81b45a9a13e19880ad78fc2d
tristankruuk <tristanhans.kruuk@techno.ee>
2026-09-09 09:34:08 +0300

    Add functions example

--- index.php
+++ index.php
@@ -1,47 +1,82 @@
 <?php
-for($i=0; $i<10; $i++){
-    var_dump($i);
-};
 
-for($i=9; $i>=0; $i--){
-    var_dump($i);
-};
-
-
-
-for($i=2; $i<1_000_000; $i*=2){
-    var_dump($i);
-};
-
-
-
-$time = time();
-$n = 0;
-while($time+1 > time()) {
-    $n++;
-}
-var_dump($n);
-
-while(false){
-    var_dump('WHILE');
+// Kui siia toppida void asemel null, siis peab olema null, mitte väärtuse puudumine
+// Void = null / väärtuse puudumine
+// Null = null
+function hello(): void {
+    var_dump('Hello');
 }
 
-do {
-    var_dump('DO');
-} while(false);
+$hi = hello();
+var_dump($hi);
+hello();
+hello();
 
-// while(true) {
+// $name='x', kus x on default väärtus, et ei tekiks errorit
+function helloName($name='Nimetu') {
+    var_dump("Hello, $name!");
+}
 
-// }
+helloName("Tristan");
+helloName("Hans");
 
-$fruits = ['Apple', 'Cherry', 'Banana'];
+function helloNameAndAge($name, $age) {
+    var_dump("Hello. $name! You are $age years olds.");
+}
 
-foreach($fruits as $fruit) {
-    var_dump($fruit);
+helloNameAndAge('Tristan', 18);
+
+// Liiga vähe argumente
+
+/*
+helloNameAndAge('Tristan');
+helloName();
+*/
+
+$test = function (){
+
 };
 
+var_dump($test);
 
-foreach($fruits as $key=>$fruit) {
-    var_dump($fruit, $key);
-};
+// Kaardistab array
+// Korrutab iga array muutuja ($n) iseendaga ($n)
+$numbers = [1, 2, 3, 4, 5];
+$squares = array_map(function ($n) {
+    return $n * $n;
+}, $numbers);
+
+// Sama asja lühem versioon
+$squares = array_map(fn ($n) => $n * $n, $numbers);
+
+var_dump($numbers);
+var_dump($squares);
+
+function cube(int $a): int|string {
+    if($a < 0) {
+        return 'No negative!';
+    } else {
+        return $a * $a * $a;
+    }
+    var_dump('BLAAAAAAA xD');
+}
+
+// Kui if-ist edasi ei jõuta, siis lõppeb funktsioon niikuinii, seega võib else ära jätta:
+/*function cube($a) {
+    if($a < 0) {
+        return 'No negative!';
+    }
+    return $a * $a * $a;
+    var_dump('BLAAAAAAA xD');
+}
+*/
+
+// Lihtne var_dump on üks võimalus returni kasutada
+var_dump(cube(4));
+
+// Returni üks kasututs on võimalus kasutada seda teises funktsioonis
+$answer = cube(5);
+$text = "Cube of 5 is $answer!";
+echo $text;
+
 ?>
