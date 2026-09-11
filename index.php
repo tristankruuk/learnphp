7cf9365f8bd2340d56ccc37203223863e7ece04e
tristankruuk <tristanhans.kruuk@techno.ee>
2026-09-11 14:49:11 +0300

    Add php templating example

--- index.php
+++ index.php
@@ -1,45 +1,45 @@
 <?php
 
-// library
-
-class Job {
-    public function task(ConsoleLogger|NothingLogger $logger) {
-        for ($i = 0; $i < 10; $i++) {
-            // Mingisugune task tehakse (faili laadimine vmidaiganes)
-            $logger->log("Task $i was done!");
-        }
-    }
-}
-
-class ConsoleLogger implements Logger {
-    public function log($message) {
-        echo "$message\n";
-    }
-}
-
-class NothingLogger implements Logger {
-    public function log($message) {
-
-    }
-}
-
-interface Logger {
-    public function log($message);
-}
-
-
-// user code
-
-class FileLogger implements Logger {
-    public function log($message) {
-        $file = fopen('log.txt', 'a');
-        fwrite($file, "$message\n");
-        fclose($file);
-    }
-}
-
-$job = new Job();
-$logger = new NothingLogger();
-$job->task($logger);
+$name= 'Tristan';
+$coinflip = rand(0, 1);
 
 ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+
+
+<body>
+    <!--
+    <h1>Hello < ?php echo = $name?>!</h1>
+
+    Saab muuta ümber:
+    -->
+    <h1>Hello <?=$name?>!</h1>
+
+
+    <?php if($coinflip): ?>
+        <h1>Kull</h1>
+    <?php else: ?>
+        <h1>Kiri</h1>
+    <?php endif ?>
+
+
+    <ul>
+        <?php for ($i=0;$i<10;$i++): ?>
+            <li><?=$i?></li>
+        <?php endfor ?>
+    </ul>
+</body>
+</html>
