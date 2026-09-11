8729aad9a5cb71b979b6915f2afaa900716cf62b
tristankruuk <tristanhans.kruuk@techno.ee>
2026-09-11 14:15:16 +0300

    Add interfaces example

--- index.php
+++ index.php
@@ -11,23 +11,26 @@ class Job {
     }
 }
 
-class ConsoleLogger {
+class ConsoleLogger implements Logger {
     public function log($message) {
         echo "$message\n";
     }
 }
 
-class NothingLogger {
+class NothingLogger implements Logger {
     public function log($message) {
 
     }
 }
 
+interface Logger {
+    public function log($message);
+}
 
 
 // user code
 
-class FileLogger {
+class FileLogger implements Logger {
     public function log($message) {
         $file = fopen('log.txt', 'a');
         fwrite($file, "$message\n");
@@ -38,9 +41,5 @@ class FileLogger {
 $job = new Job();
 $logger = new NothingLogger();
 $job->task($logger);
-
-
-
-// Kasutajatel on palju soove ja nad tahavad oma lahendusi. Pole mõtet lasta neil koodi kirjutada ja kogu koodi mega pikaks lasta, selle asemel saab kasutada interface
 
 ?>
